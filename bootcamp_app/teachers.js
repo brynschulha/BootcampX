d0bc03fd0a1ec8577d70a817a63e8c0ae6f13f44
Bryn <bschulha@ualberta.ca>
2020-10-15 02:58:46 +0000

    Changed our app to protect against SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+// pool.query(`
+// SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
+// FROM teachers
+// JOIN assistance_requests
+// ON teachers.id = assistance_requests.teacher_id
+// JOIN students
+// ON students.id = assistance_requests.student_id
+// JOIN cohorts 
+// ON cohorts.id = students.cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
+
+const queryString = `
 SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests
@@ -16,9 +34,12 @@ JOIN students
 ON students.id = assistance_requests.student_id
 JOIN cohorts 
 ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
